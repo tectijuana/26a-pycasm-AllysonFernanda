b96f1c48110aafe30cfac81384a18ff6049601a4
AllysonFernanda <l23210630@tectijuana.edu.mx>
2026-04-15 17:33:59 -0700

    Update app.py

--- codigo/app.py
+++ codigo/app.py
@@ -1,30 +1,20 @@
 import ctypes
 import time
-import random
 
 lib = ctypes.CDLL("./build/libops.so")
 
-lib.suma.argtypes = [ctypes.c_long, ctypes.c_long]
-lib.suma.restype = ctypes.c_long
+# Configuración tipos
+lib.add.argtypes = [ctypes.c_long, ctypes.c_long]
+lib.add.restype = ctypes.c_long
 
-print("Suma ASM:", lib.suma(10, 20))
+print("ADD:", lib.add(10, 20))
 
+# Benchmark
+def benchmark():
+    N = 10_000_000
+    start = time.time()
+    for _ in range(N):
+        lib.add(1,2)
+    print("ASM:", time.time() - start)
 
-# Benchmark simple
-def test_python(arr):
-    return sum(arr)
-
-def test_asm(arr):
-    ptr = (ctypes.c_long * len(arr))(*arr)
-    return lib.suma_array(ptr, len(arr))
-
-
-arr = [random.randint(1, 100) for _ in range(100000)]
-
-start = time.time()
-test_python(arr)
-print("Python:", time.time() - start)
-
-start = time.time()
-test_asm(arr)
-print("ASM:", time.time() - start)
+benchmark()
